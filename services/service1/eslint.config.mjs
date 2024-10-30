1c44f8faf34b1482df5dcc2359c7cb3a54d7350a
Takuma Ikeda <takuma.ikeda@hpe.com>
2024-10-30 11:03:58 +0900

    Add: service1のCI/CDパイプラインを追加

--- services/service1/eslint.config.mjs
+++ services/service1/eslint.config.mjs
@@ -5,12 +5,12 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default [
-    { ignores: ['build/*'] },
-    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-    { settings: { react: { version: 'detect' } } },
-    pluginJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    eslintConfigPrettier,
+  { ignores: ['build/*'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { settings: { react: { version: 'detect' } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ]
